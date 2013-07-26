693a4e2a78cb2a67611ad206bbf67226cf17d89e
Nathan Barry <nathan@thinklegend.com>
2013-07-26 11:16:38 -0600

    Added readme and new embed code

--- convertkit_form.php
+++ convertkit_form.php
@@ -1,19 +1,18 @@
 <?php
 	function generateFormHTML($data, $form_id) {
 	  if ($data->subscription_form->image->url && $data->subscription_form->image->url != "/images/standard/missing.png" && $data->subscription_form->image->url != "") {
-	    $img_string = '<img alt="Dismissrr_(20130110)" src="' . $data->subscription_form->image->url . '" />';
+	    $img_string = '<img alt="" src="' . $data->subscription_form->image->url . '" />';
     } else {
       $img_string = '';
     }
-		$returnMsgPart1 = '<div class="ck_subscription_form ck_horizontal_subscription_form">
-	              	 <div class="ck_subscription_form_content">
-	  	<h3 class="ck_subscription_form_title">' . $data->subscription_form->title . '</h3>
-	  	<span class="ck_image">' . $img_string . '</span>
-	  	<div class="ck_subscription_description">' . $data->subscription_form->description . '</div>
+		$returnMsgPart1 = '<div class="ck_embed_form ck_horizontal_subscription_form">
+	              	 <div class="ck_embed_form_content">
+	  	<h3 class="ck_embed_form_title">' . $data->subscription_form->title . '</h3>
+	  	<div class="ck_embed_description"><span class="ck_image">' . $img_string . '</span>' . $data->subscription_form->description . '</div>
 	</div>  	
 	    <p id="ck_success_msg" style="display: none; opacity: 0;">' . $data->subscription_form->success_message . '</p>
 	  	<!--  Form starts here	-->
-	  	<form id="ck_subscribe_form" class="ck_subscribeForm" action="https://convertkit.com/app/landing_pages/' . $form_id . '/subscribeFromId">
+	  	<form id="ck_subscribe_form" class="ck_subscribe_form" action="https://convertkit.com/app/landing_pages/' . $form_id . '/subscribeFromId">
 	  	  <input type="hidden" name="id" value="' . $form_id . '" id="landing_page_id"></input>
 	  	  <p class="ck_errorArea"></p>
 	  		<div class="ck_control_group">
@@ -30,134 +29,186 @@
 			$returnMsgPart2 = "<label class=\"ck_checkbox\" style=\"display:none;\"><input class=\"optIn ck_course_opted\" type=\"checkbox\" id=\"optIn\" /></label>";
 		}
 		$returnMsgPart3 = '<button class="subscribe_button ck_subscribe_button btn fields">' . $data->subscription_form->sign_up_button_text . '</button>
+			<span class="ck_guarantee">We won\'t send you spam. Unsubscribe at any time.</span>
 	  	</form>
-	  	<span class="ck_guarantee">We won\'t send you spam. Unsubscribe at any time.</span>
 	  </div>
 	  <script src="https://convertkit.com/app/assets/CKJS.js"></script>
 	  <script src="https://convertkit.com/app/assets/CKJS_UI.js"></script>
 	<style type="text/css">
-	.ck_subscription_form {
-		background: #fff;
-		-webkit-border-radius: 5px;
-		-moz-border-radius: 5px;
-		border-radius: 5px;
-		border: 1px solid #dbdbdb;
-		overflow: hidden;
-	}	
+.ck_embed_form {
+	overflow: hidden;
+	font-family: \'Helvetica Neue\', Helvetica, Arial, Verdana, sans-serif;
+	font-size: 14px;
+	border: 1px solid #d8d8d8; /* stroke */
+	background-color: #f6f4f5; /* layer fill content */
+	-moz-box-shadow: 0 1px 5px rgba(0,0,0,.1), inset 0 1px 0 rgba(255,255,255,1); /* drop shadow and inner shadow */
+	-webkit-box-shadow: 0 1px 5px rgba(0,0,0,.1), inset 0 1px 0 rgba(255,255,255,1); /* drop shadow and inner shadow */
+	box-shadow: 0 1px 5px rgba(0,0,0,.1), inset 0 1px 0 rgba(255,255,255,1); /* drop shadow and inner shadow */
+	color: #383838;
+}	
 
-	.ck_horizontal_subscription_form {
-		width: 560px;
-		padding: 20px 20px 20px;
-	}	
+.ck_horizontal_subscription_form {
+	width: 598px;
+}	
 
 
-	.ck_vertical_subscription_form {
-		width: 320px;
-		padding: 30px 40px 40px;
-	}	
+.ck_vertical_subscription_form {
+	width: 348px;
+}	
 
-	.ck_horizontal_subscription_form .ck_subscription_form_content {
+.ck_embed_form_content {
+	padding: 30px;	
+	background: #f8f8f8;
+}
+
+.ck_horizontal_subscription_form .ck_embed_form_content {
+	float: left;
+	width: 240px;
+	border-right: 1px solid #e4e4e4;
+}
+
+.ck_vertical_subscription_form .ck_embed_form_content {
+	border-bottom: 1px solid #e4e4e4;
+	-webkit-box-shadow: 0 1px 0 rgba(255,255,255,1);
+	-moz-box-shadow: 0 1px 0 rgba(255,255,255,1);
+	box-shadow: 0 1px 0 rgba(255,255,255,1);
+}
+
+
+.ck_subscribe_form {
+	padding: 25px 30px 30px;
+}
+	
+.ck_horizontal_subscription_form .ck_subscribe_form {
+	float: right;
+	width: 235px;
+}
+
+.ck_embed_form .ck_embed_form_title {
+		color: #222;
+		margin-top: 0px;
+		padding: 0px;
+		font-weight: 400;
+		font-size: 24px;
+		
+	}
+	
+.ck_embed_form .ck_image {
+		text-align: center;
 		float: left;
-		width: 310px;
-		margin-right: 10px;
+		margin: 0px 10px 5px 0px;
 	}
-		
-	.ck_horizontal_subscription_form .ck_subscribeForm {
-		float: right;
-		width: 240px;
+	
+.ck_embed_form .ck_embed_description {
 	}
 
-	.ck_subscription_form .ck_subscription_form_title {
-			color: #222;
-			margin-top: 0px;
-			padding: 0px;
-			font-weight: 400;
-			font-size: 22px;
-		}
-		
-	.ck_subscription_form .ck_image {
-			width: 150px;
-			text-align: center;
-			float: left;
-		}
-		
-	.ck_subscription_form .ck_subscription_description {
-			float: left;
-			margin-left: 10px;
-			width: 150px;
-		}
-		
-	.ck_subscription_form input[type="text"], .ck_subscription_form input[type="text"] {
-		font-size: 14px;
-		border: 1px solid #ddd;
-		-webkit-border-radius: 5px;
-		-moz-border-radius: 5px;
-		border-radius: 5px;
-		padding:  5px;
-		width: 240px;
-	}	
+.ck_subscribe_form {
+	
+}
 
-	.ck_subscription_form input[type="text"]:focus, .ck_subscription_form input[type="text"]:focus {
-		outline: none;
-		border-color: #aaa;
-	}	
+.ck_horizontal_subscription_form input[type="text"] {
+	margin-bottom: 10px;
+}	
 
-	/* Horizontal Styles */	
-	.ck_horizontal_subscription_form .ck_label {
-			font-weight: bold;
-			display: block;
-			}
-			
-	.ck_subscription_form .ck_form-horizontal.ck_controls {
-				margin-left: 110px;
-			}
-			
-	/* Vertical Styles */
+.ck_embed_form input[type="text"], .ck_embed_form input[type="email"] {
+	font-size: 14px;
+	padding: 10px 8px;
+	width: 220px;
+	border: 1px solid #dfdfdf; /* stroke */
+	-moz-border-radius: 4px;
+	-webkit-border-radius: 4px;
+	border-radius: 4px; /* border radius */
+	-moz-background-clip: padding;
+	-webkit-background-clip: padding-box;
+	background-clip: padding-box; /* prevents bg color from leaking outside the border */
+	background-color: #fff; /* layer fill content */
+	-moz-box-shadow: inset 0 1px 3px rgba(0,0,0,.11); /* inner shadow */
+	-webkit-box-shadow: inset 0 1px 3px rgba(0,0,0,.11); /* inner shadow */
+	box-shadow: inset 0 1px 3px rgba(0,0,0,.11); /* inner shadow */
+	
+}	
 
-	.ck_vertical_subscription_form #ck_subscribe_form {
-		clear: both;
-		padding-top: 20px;
-		}	
-					
-	.ck_vertical_subscription_form .ck_label {
+.ck_embed_form input[type="text"]:focus, .ck_embed_form input[type="email"]:focus {
+	outline: none;
+	border-color: #aaa;
+}	
+
+.ck_label {
 		font-weight: bold;
 		display: block;
-		width:  100px;
-		float: left;
-		text-align: right;
-		height: 32px;
-		line-height: 32px;
+		font-size: 12px;
+		color: #565656;
+		text-transform: uppercase;
 		}
-					
-	.ck_vertical_subscription_form input[type="text"], .ck_vertical_subscription_form input[type="text"] {
-		width: 200px;
-		float: right;
-		}	
+		
+		
+/* Vertical Styles */
 
-	.ck_checkbox {
-		padding: 10px 0px 10px 20px;
+.ck_vertical_subscription_form .ck_subscribe_form {
+	clear: both;
+	padding-top: 20px;
+	}	
+				
+.ck_vertical_subscription_form .ck_label {
+	font-weight: bold;
+	display: block;
+	width: 100px;
+	float: left;
+	text-align: right;
+	height: 42px;
+	line-height: 42px;
+	}
+				
+.ck_vertical_subscription_form input[type="text"], .ck_vertical_subscription_form input[type="email"] {
+	width: 160px;
+	float: right;
+	}	
+
+.ck_checkbox {
+	padding: 10px 0px 10px 20px;
+	display: block;
+	clear: both;
+}
+
+.ck_checkbox input.optIn {
+	margin-left:  -20px;
+}
+	
+.ck_embed_form .ck_subscribe_button {
+		width: 100%;
+		color: #fff;
+		text-shadow: 0 -1px 0 rgba(0,0,0,.43);
+		margin: 0px 0px 0px;
+		padding:  10px 5px;
+		font-size: 18px;
+		border: 1px solid #0d6db7; /* stroke */
+		-moz-border-radius: 4px;
+		-webkit-border-radius: 4px;
+		border-radius: 4px; /* border radius */
+		-moz-background-clip: padding;
+		-webkit-background-clip: padding-box;
+		background-clip: padding-box; /* prevents bg color from leaking outside the border */
+		background-color: #0d6db8; /* layer fill content */
+		-moz-box-shadow: inset 0 1px 0 rgba(255,255,255,.33); /* inner shadow */
+		-webkit-box-shadow: inset 0 1px 0 rgba(255,255,255,.33); /* inner shadow */
+		box-shadow: inset 0 1px 0 rgba(255,255,255,.33); /* inner shadow */
+		background-image: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEwMCAxMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxsaW5lYXJHcmFkaWVudCBpZD0iaGF0MCIgZ3JhZGllbnRVbml0cz0ib2JqZWN0Qm91bmRpbmdCb3giIHgxPSI1MCUiIHkxPSIxMDAlIiB4Mj0iNTAlIiB5Mj0iLTEuNDIxMDg1NDcxNTIwMmUtMTQlIj4KPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iIzAwMCIgc3RvcC1vcGFjaXR5PSIwLjEiLz4KPHN0b3Agb2Zmc2V0PSIxMDAlIiBzdG9wLWNvbG9yPSIjZmZmIiBzdG9wLW9wYWNpdHk9IjAuMSIvPgogICA8L2xpbmVhckdyYWRpZW50PgoKPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEwMCIgaGVpZ2h0PSIxMDAiIGZpbGw9InVybCgjaGF0MCkiIC8+Cjwvc3ZnPg==); /* gradient overlay */
+		background-image: -moz-linear-gradient(bottom, rgba(0,0,0,.1) 0%, rgba(255,255,255,.1) 100%); /* gradient overlay */
+		background-image: -o-linear-gradient(bottom, rgba(0,0,0,.1) 0%, rgba(255,255,255,.1) 100%); /* gradient overlay */
+		background-image: -webkit-linear-gradient(bottom, rgba(0,0,0,.1) 0%, rgba(255,255,255,.1) 100%); /* gradient overlay */
+		background-image: linear-gradient(bottom, rgba(0,0,0,.1) 0%, rgba(255,255,255,.1) 100%); /* gradient overlay */
+		cursor: pointer;
+	}
+	
+.ck_embed_form .ck_guarantee {
+		color: #777;
+		font-size: 10px;
+		clear: left;
 		display: block;
-		clear: both;
+		padding: 15px 0px 0px;
+		text-align: center;
 	}
 
-	.ck_checkbox input.optIn {
-		margin-left:  -20px;
-	}
-		
-	.ck_subscription_form .ck_subscribe_button {
-			width: 100%;
-			margin: 0px 0px 0px;
-			padding-top: 15px;
-			padding-bottom: 15px;
-			font-size: 18px;
-		}
-		
-	.ck_subscription_form .ck_guarantee {
-			color: #777;
-			font-size: 13px;
-			clear: left;
-			display: block;
-		}
 	</style>';
 		
 		$returnMsg = $returnMsgPart1 . $returnMsgPart2 . $returnMsgPart3;
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== ConvertKit ===
+Contributors: Nathan Barry, Sam Baumgarten
+Donate link: https://convertkit.com
+Tags: email, marketing, embed form, convertkit, capture
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+ConvertKit is an email marketing platform for capturing leads from your WordPress blog. 
+
+== Description ==
+
+ConvertKit is an email marketing platform for capturing leads from your WordPress blog. You can create unique forms for each post, then easily create email courses to drip out to your subscribers.
+
+This plugin integrates ConvertKit.com accounts with your WordPress blog.
+
+== Installation ==
+
+1. Upload the `ConvertKit_WordPress` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Find the ConvertKit API key under the settings tab. Then copy your key from the ConvertKit.com account screen
+4. Select a default form to appear on all posts
+5. If desired, override the default form on each individual post by editing that post
+
+== Frequently asked questions ==
+
+= Does this plugin require a paid service? =
+
+Yes, for it to work you must first have an account on ConvertKit.com
+
+== Screenshots ==
+
+1. https://www.dropbox.com/s/6fgcuupqrmrs4zu/ConvertKit-Embed.png
+2. https://www.dropbox.com/s/mo0yjawwbtb9qn9/ConvertKit-Embed-Vertical.png
+
+== Changelog ==
+
+
+
+== Upgrade notice ==
+
